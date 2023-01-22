f40f0b70ad5f616edfcd90db7aae075b7594b783
Jeremy Hanson <rockbasser88@hotmail.com>
2023-01-21 19:47:41 -0600

    setup server GET and POST routes

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+
+// GET /feedback
+router.get('/', (req, res) => {
+  let queryText = 'SELECT * FROM "feedback" ORDER BY "date";';
+  pool.query(queryText).then(result => {
+    // Sends back the results in an object
+    res.send(result.rows);
+  })
+  .catch(error => {
+    console.log('error getting books', error);
+    res.sendStatus(500);
+  });
+});
+
+// POST /feedback
+router.post('/',  (req, res) => {
+  let newFeedback = req.body;
+  console.log(`Adding feedback`, newFeedback);
+
+  let queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+  VALUES ($1, $2, $3, $4);`;
+  pool.query(queryText, [
+    newFeedback.feeling, 
+    newFeedback.understanding,
+    newFeedback.support,
+    newFeedback.comments
+    ])
+    .then(result => {
+      res.sendStatus(201);
+    })
+    .catch(error => {
+      console.log(`Error adding new feedback`, error);
+      res.sendStatus(500);
+    });
+});
+
+
+module.exports = router;
